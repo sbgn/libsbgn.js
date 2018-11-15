587b9b4a8156530b8a112c90f600d5277bb46a44
utku <calisutku@gmail.com>
2018-11-15 23:57:39 +0300

    capture stack error fix

--- src/test-files/schematronValidatorTest.js
+++ src/test-files/schematronValidatorTest.js
@@ -1,4 +1,4 @@
-var schematronValidator = require('./SchematronValidator');
+var schematronValidator = require('./schematronValidator');
 var fs = require('file-system');
 var file=fs.readFileSync('pd10112-fail-1.sbgn', 'utf8');
 var result = schematronValidator.isValid(file);
