cbada60c9b49fe9aebc4aca4fda7d972495d6840
ucalis <calisutku@gmail.com>
2018-12-28 18:38:08 +0300

    ajax req server side done

--- src/schematronValidator.js
+++ src/schematronValidator.js
@@ -2,47 +2,30 @@ var ns = {};
 var fs = require('fs');
 var xml2js = require('xml2js');
 var Issue =  require('./Issue').Issue;
+var libxslt = require('libxslt');
 var SchematronValidation = function(file) {
 	this.file 	= file;
 };
 SchematronValidation.isValid = function(file) {
 	try {
-  		var isoContent= loadXMLDoc("node_modules/libsbgn.js/src/template.xslt");
-		var xml = new DOMParser().parseFromString(file, "text/xml");
-		var xsltProcessor = new XSLTProcessor();
-	        var result ;
-		if (window.ActiveXObject || xhttp.responseType == "msxml-document")
- 		 {
-  			result = xml.transformNode(xsl);
-  		}
-		// code for Chrome, Firefox, Opera, etc.
-		else if (document.implementation && document.implementation.createDocument)
- 		 {
- 			 xsltProcessor = new XSLTProcessor();
-  		         xsltProcessor.importStylesheet(isoContent);
- 			 result = xsltProcessor.transformToFragment(xml, document);
-  		}
-		var tmp = document.createElement("div");
-		tmp.appendChild(result);
-		result = tmp.innerHTML; 
-		var parseString = xml2js.parseString;
-		var parsedResult;
-		parseString(result, function (err, data) {
-    			parsedResult = data;
-		});
+		var isoContent=fs.readFileSync('template.xslt', 'utf8');
+		var stylesheet2 =libxslt.parse(isoContent);
+		var resultDocument2 = stylesheet2.apply(file);
+		 parser.parseString(resultDocument2, function (err, result) {
+        		resultDocument2 = result;
+    		})
 		var errors = [];
-		if(parsedResult["svrl:schematron-output"]["svrl:failed-assert"] == undefined)
+		if(resultDocument2["svrl:schematron-output"]["svrl:failed-assert"] == undefined)
 			return errors;
 		var errCount= resultDocument2["svrl:schematron-output"]["svrl:failed-assert"].length;
 		for(var i=0;i<errCount;i++){
 		   var error = new Issue();
-		   error.setText(parsedResult["svrl:schematron-output"]["svrl:failed-assert"][i]["svrl:text"]);
-		   error.setPattern(parsedResult["svrl:schematron-output"]["svrl:failed-assert"][i]["$"]["id"]); 
-		   error.setRole(parsedResult["svrl:schematron-output"]["svrl:failed-assert"][i]["svrl:diagnostic-reference"][0]["_"]);	
+		   error.setText(resultDocument2["svrl:schematron-output"]["svrl:failed-assert"][i]["svrl:text"]);
+		   error.setPattern(resultDocument2["svrl:schematron-output"]["svrl:failed-assert"][i]["$"]["id"]); 
+		   error.setRole(resultDocument2["svrl:schematron-output"]["svrl:failed-assert"][i]["svrl:diagnostic-reference"][0]["_"]);	
 		   errors.push(error);	 			
 		}
 					
-		//console.log(result["svrl:schematron-output"]["svrl:failed-assert"][0]);
 		return errors;
 	}
 	catch(e) {
--- src/test-files/schematronValidatorTest.js
+++ src/test-files/schematronValidatorTest.js
@@ -1,5 +1,5 @@
 var schematronValidator = require('./schematronValidator');
 var fs = require('file-system');
-var file=fs.readFileSync('pd10112-fail-1.sbgn', 'utf8');
+var file=fs.readFileSync('pd10101-fail.sbgn.xml', 'utf8');
 var result = schematronValidator.isValid(file);
 console.log(result);
