8e25eb0dc22d9f9563a539a0d15fd0b38f404058
ucalis <calisutku@gmail.com>
2019-01-28 20:22:56 +0300

    web service called

--- src/schematronValidator.js
+++ src/schematronValidator.js
@@ -1,68 +1,27 @@
 var ns = {};
 var xml2js = require('xml2js');
-var Issue =  require('./Issue').Issue;
 var SchematronValidation = function(file) {
 	this.file 	= file;
 };
 SchematronValidation.isValid = function(file) {
 	try {
-  		var isoContent= loadXMLDoc("node_modules/libsbgn.js/src/template.xslt");
-		var xml = new DOMParser().parseFromString(file, "text/xml");
-		var xsltProcessor = new XSLTProcessor();
-	        var result ;
-		if (window.ActiveXObject || xhttp.responseType == "msxml-document")
- 		 {
-  			result = xml.transformNode(xsl);
-  		}
-		// code for Chrome, Firefox, Opera, etc.
-		else if (document.implementation && document.implementation.createDocument)
- 		 {
- 			 xsltProcessor = new XSLTProcessor();
-  		         xsltProcessor.importStylesheet(isoContent);
- 			 result = xsltProcessor.transformToFragment(xml, document);
-  		}
-		var tmp = document.createElement("div");
-		tmp.appendChild(result);
-		result = tmp.innerHTML; 
-		var parseString = xml2js.parseString;
-		var parsedResult;
-		parseString(result, function (err, data) {
-    			parsedResult = data;
-		});
-		var errors = [];
-		if(parsedResult["svrl:schematron-output"]["svrl:failed-assert"] == undefined)
-			return errors;
-		var errCount= resultDocument2["svrl:schematron-output"]["svrl:failed-assert"].length;
-		for(var i=0;i<errCount;i++){
-		   var error = new Issue();
-		   error.setText(parsedResult["svrl:schematron-output"]["svrl:failed-assert"][i]["svrl:text"]);
-		   error.setPattern(parsedResult["svrl:schematron-output"]["svrl:failed-assert"][i]["$"]["id"]); 
-		   error.setRole(parsedResult["svrl:schematron-output"]["svrl:failed-assert"][i]["svrl:diagnostic-reference"][0]["_"]);	
-		   errors.push(error);	 			
-		}
-					
-		//console.log(result["svrl:schematron-output"]["svrl:failed-assert"][0]);
-		return errors;
+ 		 $.ajax({
+	      type: 'post',
+	      url: "/schematronValidator/isValid",
+	      port : '8081'
+	      data: {file: file},
+	      success: function(data){
+		return data;
+	      },
+	      error: function(req, status, err) {
+		console.error("Error during file validation", status, err);
+	      }
+	    });
 	}
 	catch(e) {
 		console.log(e);
-		return false;
+		return null;
 	}	
-}
-function loadXMLDoc(filename)
-{
-if (window.ActiveXObject)
-  {
-  xhttp = new ActiveXObject("Msxml2.XMLHTTP");
-  }
-else 
-  {
-  xhttp = new XMLHttpRequest();
-  }
-xhttp.open("GET", filename, false);
-try {xhttp.responseType = "msxml-document"} catch(err) {} // Helping IE11
-xhttp.send("");
-return xhttp.responseXML;
 }
 ns.SchematronValidation = SchematronValidation;
 module.exports = ns;
--- src/test-files/schematronValidatorTest.js
+++
@@ -1,5 +0,0 @@
-var schematronValidator = require('./schematronValidator');
-var fs = require('file-system');
-var file=fs.readFileSync('pd10101-fail.sbgn.xml', 'utf8');
-var result = schematronValidator.isValid(file);
-console.log(result);
