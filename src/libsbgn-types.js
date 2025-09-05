7a01377ee0c8efb7c9dbe2009b87cde3a0980f8a
Sinan Havus <sinanhavus@gmail.com>
2025-09-05 11:02:18 +0300

    Rename 'emptyset' to 'empty set'

--- src/libsbgn-types.js
+++ src/libsbgn-types.js
@@ -26,7 +26,7 @@ ns.GlyphClass = Object.freeze({
     COMPLEX: "complex",
     COMPLEX_MULTIMER: "complex multimer",
     SOURCE_AND_SINK: "source and sink",
-    EMPTYSET: "emptyset",
+    EMPTY_SET: "empty set",
     PERTURBATION: "perturbation",
     BIOLOGICAL_ACTIVITY: "biological activity",
     PERTURBING_AGENT: "perturbing agent",
